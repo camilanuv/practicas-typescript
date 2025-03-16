3246dd440294a476938154096d4c013bb6d73ee4
Laura Camila Nuvan <camilanuvan@gmail.com>
2025-03-16 13:05:48 -0500

    Implementado contador

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import Button from './components/Button'
+import Counter from './components/Counter'
+
+function greet(name: string): string {
+  return `Hola ${name}`
+}
+
 
 function App() {
   const handleClick = () => alert("Ya diste click")
+  greet("Laura Camila")
 
   return (
-   <Button onclick={handleClick} label="Click me"/>
+    <>
+      <Button onclick={handleClick} label="Click me"/>
+      <Counter/>   
+    </>
+   
   )
 }
 
